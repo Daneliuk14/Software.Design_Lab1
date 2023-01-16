6c90bc08bd1c6dcb14aa8b5143ce32b1785b9053
Daniel <rokas.briauka@stud.vilniustech.lt>
2023-01-16 20:29:52 +0200

    new file:   Labwork_1.csproj 	new file:   Program.cs 	new file:   bin/Debug/net6.0/Labwork_1.deps.json 	new file:   bin/Debug/net6.0/Labwork_1.dll 	new file:   bin/Debug/net6.0/Labwork_1.exe 	new file:   bin/Debug/net6.0/Labwork_1.pdb 	new file:   bin/Debug/net6.0/Labwork_1.runtimeconfig.json 	new file:   obj/Debug/net6.0/.NETCoreApp,Version=v6.0.AssemblyAttributes.cs 	new file:   obj/Debug/net6.0/Labwork_1.AssemblyInfo.cs 	new file:   obj/Debug/net6.0/Labwork_1.AssemblyInfoInputs.cache 	new file:   obj/Debug/net6.0/Labwork_1.GeneratedMSBuildEditorConfig.editorconfig 	new file:   obj/Debug/net6.0/Labwork_1.GlobalUsings.g.cs 	new file:   obj/Debug/net6.0/Labwork_1.assets.cache 	new file:   obj/Debug/net6.0/Labwork_1.csproj.AssemblyReference.cache 	new file:   obj/Debug/net6.0/Labwork_1.csproj.CoreCompileInputs.cache 	new file:   obj/Debug/net6.0/Labwork_1.csproj.FileListAbsolute.txt 	new file:   obj/Debug/net6.0/Labwork_1.dll 	new file:   obj/Debug/net6.0/Labwork_1.genruntimeconfig.cache 	new file:   obj/Debug/net6.0/Labwork_1.pdb 	new file:   obj/Debug/net6.0/apphost.exe 	new file:   obj/Debug/net6.0/ref/Labwork_1.dll 	new file:   obj/Debug/net6.0/refint/Labwork_1.dll 	new file:   obj/Labwork_1.csproj.nuget.dgspec.json 	new file:   obj/Labwork_1.csproj.nuget.g.props 	new file:   obj/Labwork_1.csproj.nuget.g.targets 	new file:   obj/project.assets.json 	new file:   obj/project.nuget.cache

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
+namespace Dan1;
+class Program
+{
+   static void Main(string[] args)
+  {
+    using (var client = new WebClient())
+        {
+            client.DownloadFile("https://www.registrucentras.lt/aduomenys/?byla=01_gr_open_amzius_lytis_pilietybes_sav_r1.csv", "01_gr_open_amzius_lytis_pilietybes_sav_r1.csv");
+        }
+    List<string> needed_values = new List<string>();
+    List<string> needed_values_licence = new List<string>();
+
+    using(var reader = new StreamReader(@".\01_gr_open_amzius_lytis_pilietybes_sav_r1.csv"))
+    {
+      while (!reader.EndOfStream)
+      {
+        var line = reader.ReadLine();
+        var values = line.Split(',');
+        var without_qoutes = values[7].Trim(' ', ' ', '\t');
+        var street = without_qoutes.Split('.');
+
+        needed_values.Add(street[0]);
+      }
+
+        var counts = needed_values.GroupBy(w => w).Select(g => new {district = g.Key, Count = g.Count()}).ToList();
+
+        List<int> number_repetitions = new List<int>();
+        List<int> usable = new List<int>();
+
+        foreach(var p in counts) 
+        {
+            number_repetitions.Add(p.Count);
+        }
+
+        var descendingOrder = number_repetitions.OrderByDescending(i => i);
+
+        foreach(var p in descendingOrder) 
+        {
+            usable.Add(p);
+        }
+        for(int i = 0; i < 5; i++)
+        {
+            foreach(var p in counts)
+            {
+            
+                if(p.Count == usable[i])
+                {
+                    Console.WriteLine(p);
+                }
+            }
+
+        }
+
+    }
+
+  }
+
+}
